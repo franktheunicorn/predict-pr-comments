92dbb497120d49b7a1f4033d8212deab3af7b5af
Holden Karau <holden@pigscanfly.ca>
2018-12-19 14:24:18 -0800

    Initial kf_app generation

---
+++ kf_app/env.sh
@@ -0,0 +1,7 @@
+PLATFORM=gke
+KUBEFLOW_REPO=/home/hkarau/repos/kubeflow
+KUBEFLOW_VERSION=0.3.4
+KUBEFLOW_KS_DIR=/home/hkarau/repos/predict-pr-comments/kf_app/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_PLATFORM=null
