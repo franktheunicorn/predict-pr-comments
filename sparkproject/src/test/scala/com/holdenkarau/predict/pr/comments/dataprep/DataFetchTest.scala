6395cfcd1bd64d62ee7f76699404ef7d6961484e
Holden Karau <holden@pigscanfly.ca>
2019-01-15 16:56:57 -0800

    Move the sample data up

--- sparkproject/src/test/scala/com/holdenkarau/predict/pr/comments/dataprep/DataFetchTest.scala
+++ sparkproject/src/test/scala/com/holdenkarau/predict/pr/comments/dataprep/DataFetchTest.scala
@@ -10,13 +10,15 @@ import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
 class DataFetchTest extends FunSuite with SharedSparkContext {
+  val standardInputList = List(
+    "pull_request_url,pull_patch_url,comments_positions_space_delimited,comments_original_positions_space_delimited",
+    "https://api.github.com/repos/mick-warehime/sixth_corp/pulls/61,https://github.com/mick-warehime/sixth_corp/pull/61.patch,4 37 35 35 38 4 37 35 38,4 37 35 35 38 4 37 35 38")
+
+
   test("calling with a local file fetches a result") {
     val session = SparkSession.builder().getOrCreate()
     import session.implicits._
-    val inputRDD = sc.parallelize(List(
-      "pull_request_url,pull_patch_url,comments_positions_space_delimited,comments_original_positions_space_delimited",
-      "https://api.github.com/repos/mick-warehime/sixth_corp/pulls/61,https://github.com/mick-warehime/sixth_corp/pull/61.patch,4 37 35 35 38 4 37 35 38,4 37 35 35 38 4 37 35 38"
-    ))
+    val inputRDD = sc.parallelize(standardInputList)
     val inputData = session.read.option("header", "true").csv(session.createDataset(inputRDD)).as[InputData]
     val cachedData = session.emptyDataset[StoredPatch]
     val dataFetch = new DataFetch(sc)
@@ -26,10 +28,7 @@ class DataFetchTest extends FunSuite with SharedSparkContext {
   test("cache records are filtered out") {
     val session = SparkSession.builder().getOrCreate()
     import session.implicits._
-    val inputRDD = sc.parallelize(List(
-      "pull_request_url,pull_patch_url,comments_positions_space_delimited,comments_original_positions_space_delimited",
-      "https://api.github.com/repos/mick-warehime/sixth_corp/pulls/61,https://github.com/mick-warehime/sixth_corp/pull/61.patch,4 37 35 35 38 4 37 35 38,4 37 35 35 38 4 37 35 38"
-    ))
+    val inputRDD = sc.parallelize(standardInputList)
     val inputData = session.read.option("header", "true").csv(session.createDataset(inputRDD)).as[InputData]
     val basicCached = StoredPatch(
       "https://api.github.com/repos/mick-warehime/sixth_corp/pulls/61",
