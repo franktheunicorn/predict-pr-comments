a7b667293a7baa4a182b85a38c13362ffffd36e1
Kris Nova <kris@nivenly.com>
2019-01-19 16:24:11 -0800

    Adding TLS and working server and sample payload

--- pull-request-suggester/webhookserver/server.go
+++ pull-request-suggester/webhookserver/server.go
@@ -1,9 +1,16 @@
 package webhookserver
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"bytes"
+
 	"github.com/gorilla/mux"
 	"github.com/kris-nova/logger"
-	"net/http"
 )
 
 // Server options
@@ -35,13 +42,54 @@ func Serve() error {
 }
 
 func Webhook(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(formatRequest(r))
 	w.WriteHeader(200)
 	w.Write([]byte("Success: Webhook"))
 	return
 }
 
 func Auth(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(formatRequest(r))
 	w.WriteHeader(200)
 	w.Write([]byte("Success: Auth"))
 	return
+}
+
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	bodyBytes, _ := ioutil.ReadAll(r.Body)
+	bodyStr := string(bodyBytes)
+	prettyString := prettyPrint(bodyStr)
+	request = append(request, "")
+	request = append(request, prettyString)
+	// Return the request as a string
+	return strings.Join(request, "\n")
+}
+
+func prettyPrint(input interface{}) string {
+	output := &bytes.Buffer{}
+	if err := json.NewEncoder(output).Encode(input); err != nil {
+		logger.Warning("Error building encoder: %v", err)
+		return ""
+	}
+	formatted := &bytes.Buffer{}
+	if err := json.Indent(formatted, output.Bytes(), "", "  "); err != nil {
+		logger.Warning("Error indenting: %v", err)
+		return ""
+	}
+	return string(formatted.Bytes())
 }
