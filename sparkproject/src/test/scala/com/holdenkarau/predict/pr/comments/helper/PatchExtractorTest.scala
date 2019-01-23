b0ec1400fc4c7fb07c67e897be61c31ce21d89a1
Holden Karau <holden@pigscanfly.ca>
2019-01-22 16:36:54 -0800

    Fix the regexes for parsing the patches. I feel dirty writing this commit

--- sparkproject/src/test/scala/com/holdenkarau/predict/pr/comments/helper/PatchExtractorTest.scala
+++ sparkproject/src/test/scala/com/holdenkarau/predict/pr/comments/helper/PatchExtractorTest.scala
@@ -58,7 +58,27 @@ index 01aa14b..8aa21e7 100644
 
   test("Simple input") {
     val results = PatchExtractor.processPatch(simpleInput)
-    val expected = List()
+    val expected = List(
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        173,174,
+        "- SOURCE-IP-CIDR,192.168.1.201/32,DIRECT",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        35,36,
+	"	metadata := parseHTTPAddr(request)",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        35,37,
+        "	metadata.SourceIP = parseSourceIP(conn)",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        37,38,
+        "		metadata: parseHTTPAddr(request),",
+        false),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        37,39,
+        "		metadata: metadata,",
+        true))
     results should contain theSameElementsAs expected
   }
 }
