d5768f8ec7e189baec5be4b8ae9ba256f7bb7668
Holden Karau <holden@pigscanfly.ca>
2019-01-22 17:57:35 -0800

    Add an app to go through the patches and save the rejected ones so I can look at them later.

---
+++ sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/helper/PatchTestApp.scala
@@ -0,0 +1,22 @@
+package com.holdenkarau.predict.pr.comments.sparkProject.helper
+
+import org.apache.spark.sql._
+
+/**
+ * An app to run patch extraction and save invalid records in JSON for inspection.
+ */
+object PatchTestAppSC extends App {
+  val (inputFile, outputFile) = (args(0), args(1))
+  val session = SparkSession.builder.getOrCreate()
+  import session.implicits._
+  val input = session.read.format("parquet").load(inputFile).select("patch").as[String]
+  val rejected = input.flatMap {patch => 
+    try {
+      PatchExtractor.processPatch(patch)
+      None
+    } catch {
+      case e: Exception => Some((patch, e.toString))
+    }
+  }
+  rejected.write.format("json").save(outputFile)
+}
