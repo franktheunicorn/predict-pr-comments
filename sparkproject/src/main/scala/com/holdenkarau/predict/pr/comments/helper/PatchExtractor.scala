327281347e59e550a6aba172fb7209493ed0b443
Holden Karau <holden@pigscanfly.ca>
2019-01-22 17:43:24 -0800

    Add a tool to process patches (#24)
    * Add start of patch extractor: TODO -- finish regexes
    
    * Fix the regexes for parsing the patches. I feel dirty writing this commit
    
    * Fix the line numbers during patch extraction and add a multi-commit patch test.

---
+++ sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/helper/PatchExtractor.scala
@@ -0,0 +1,81 @@
+package com.holdenkarau.predict.pr.comments.sparkProject.helper
+
+import scala.util.matching.Regex
+
+case class PatchRecord(commitId: String, oldPos: Int, newNumber: Int, text: String,
+  add: Boolean)
+case class DiffRecord(newNumber: Int, text: String)
+
+object PatchExtractor {
+  val commitRegex = """^From\s([A-Za-z0-9]+)\s.*$""".r
+  val oldFilenameRegex = """^---\sa/(.+)\s?$""".r
+  val newFilenameRegex = """^\+\+\+\sb/(.+)\s?$""".r
+  // Diff command
+  val diffCommandRegex = """^diff --git a/.* b/(.*)$""".r
+  // Index
+  val indexRegex = """^index\s+(.*)\s*(.*)\s*$""".r
+  // Block header
+  val blockHeaderRegex = """^@@ -(\d+),\d+ \+(\d+),\d+ @@.*$""".r
+  // For now we only use lines that change
+  val removedLine = """^-(.*)$""".r
+  val addedLine = """^\+(.*)$""".r
+  val contextLine = """^\s.*$""".r
+
+  /*
+   * Process a given patch. You don't want to look inside this.
+   */
+  def processPatch(patch: String): Seq[PatchRecord] = {
+    val lines = patch.split("\n")
+    var commitId: String = null
+    var filename: String = null
+    var oldPos: Integer = null
+    var newPos: Integer = null
+    var seenDiff: Boolean = false
+    // Loop through the inputs
+    lines.flatMap{line =>
+      line match {
+        case diffCommandRegex(f) =>
+          seenDiff = true
+          filename = f
+          newPos = null
+          oldPos = null
+          None
+        case indexRegex(idx, fileMode) =>
+          // Do nothing
+          None
+        case commitRegex(id) =>
+          commitId = id
+          // New commit reset all the state
+          seenDiff = false
+          filename = null
+          oldPos = null
+          newPos = null
+          None
+        case oldFilenameRegex(f) if seenDiff && newPos == null =>
+          None
+        case newFilenameRegex(f) if seenDiff && newPos == null =>
+          filename = f
+          None
+        case blockHeaderRegex(op, np) if seenDiff =>
+          oldPos = op.toInt - 1
+          newPos = np.toInt - 1
+          None
+        case addedLine(lineText) if seenDiff && newPos != null =>
+          newPos = newPos + 1
+          Some(PatchRecord(commitId, oldPos, newPos, lineText, true))
+        case removedLine(lineText) if seenDiff && newPos != null =>
+          oldPos = oldPos + 1
+          Some(PatchRecord(commitId, oldPos, newPos, lineText, false))
+        case _ if seenDiff && newPos != null =>
+          // Context line
+          newPos = newPos + 1
+          oldPos = oldPos + 1
+          None
+        case _ =>
+          // Not in diff/patch view
+          None
+      }
+    }
+  }
+}
+
---
+++ sparkproject/src/test/scala/com/holdenkarau/predict/pr/comments/helper/PatchExtractorTest.scala
@@ -0,0 +1,183 @@
+package com.holdenkarau.predict.pr.comments.sparkProject.helper
+/**
+ * Test of the Patch extractor
+ */
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+class PatchExtractorTest extends FunSuite {
+  val simpleInput = """
+From 97d57259eaf8ca29ce56a194de110d526c2d1629 Mon Sep 17 00:00:00 2001
+From: songchenwen <me@songchenwen.com>
+Date: Wed, 16 Jan 2019 19:12:23 +0800
+Subject: [PATCH] Feature: SOURCE-IP-CIDR rule type
+
+---
+ README.md                  |  1 +
+ adapters/inbound/http.go   |  4 +++-
+ adapters/inbound/https.go  |  4 +++-
+ adapters/inbound/socket.go |  1 +
+ adapters/inbound/util.go   |  7 +++++++
+ config/config.go           |  4 +++-
+ constant/metadata.go       |  1 +
+ constant/rule.go           |  3 +++
+ rules/ipcidr.go            | 23 ++++++++++++++---------
+ tunnel/tunnel.go           |  6 +++---
+ 10 files changed, 39 insertions(+), 15 deletions(-)
+
+diff --git a/README.md b/README.md
+index 31c2bb2..ec256ba 100644
+--- a/README.md
++++ b/README.md
+@@ -170,6 +170,7 @@ Rule:
+ - DOMAIN,google.com,Proxy
+ - DOMAIN-SUFFIX,ad.com,REJECT
+ - IP-CIDR,127.0.0.0/8,DIRECT
++- SOURCE-IP-CIDR,192.168.1.201/32,DIRECT
+ - GEOIP,CN,DIRECT
+ # FINAL would remove after prerelease
+ # you also can use `FINAL,Proxy` or `FINAL,,Proxy` now
+diff --git a/adapters/inbound/http.go b/adapters/inbound/http.go
+index 01aa14b..8aa21e7 100644
+--- a/adapters/inbound/http.go
++++ b/adapters/inbound/http.go
+@@ -32,8 +32,10 @@ func (h *HTTPAdapter) Conn() net.Conn {
+ 
+ // NewHTTP is HTTPAdapter generator
+ func NewHTTP(request *http.Request, conn net.Conn) *HTTPAdapter {
++	metadata := parseHTTPAddr(request)
++	metadata.SourceIP = parseSourceIP(conn)
+ 	return &HTTPAdapter{
+-		metadata: parseHTTPAddr(request),
++		metadata: metadata,
+ 		R:        request,
+ 		conn:     conn,
+ 	}
+"""
+
+  test("Simple input") {
+    val results = PatchExtractor.processPatch(simpleInput)
+    val expected = List(
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        172,173,
+        "- SOURCE-IP-CIDR,192.168.1.201/32,DIRECT",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        34,35,
+	"	metadata := parseHTTPAddr(request)",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        34,36,
+        "	metadata.SourceIP = parseSourceIP(conn)",
+        true),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        36,37,
+        "		metadata: parseHTTPAddr(request),",
+        false),
+      PatchRecord("97d57259eaf8ca29ce56a194de110d526c2d1629",
+        36,38,
+        "		metadata: metadata,",
+        true))
+    results should contain theSameElementsAs expected
+  }
+  val slightlyComplexInput = """
+From a7fbc74335c2df27002e8158f8e83a919195eed7 Mon Sep 17 00:00:00 2001
+From: Holden Karau <holden@pigscanfly.ca>
+Date: Mon, 6 Aug 2018 11:04:31 -0700
+Subject: [PATCH 1/7] [SPARK-21436] Take advantage of known partioner for
+ distinct on RDDs to avoid a shuffle. Special case the situation where we know
+ the partioner and the number of requested partions output is the same as the
+ current partioner to avoid a shuffle and instead compute distinct inside of
+ each partion.
+
+---
+ core/src/main/scala/org/apache/spark/rdd/RDD.scala   | 11 ++++++++++-
+ .../test/scala/org/apache/spark/rdd/RDDSuite.scala   | 12 ++++++++++++
+ 2 files changed, 22 insertions(+), 1 deletion(-)
+
+diff --git a/core/src/main/scala/org/apache/spark/rdd/RDD.scala b/core/src/main/scala/org/apache/spark/rdd/RDD.scala
+index 0574abdca32ac..471b9e0a1a877 100644
+--- a/core/src/main/scala/org/apache/spark/rdd/RDD.scala
++++ b/core/src/main/scala/org/apache/spark/rdd/RDD.scala
+@@ -396,7 +396,16 @@ abstract class RDD[T: ClassTag](
+    * Return a new RDD containing the distinct elements in this RDD.
+    */
+   def distinct(numPartitions: Int)(implicit ord: Ordering[T] = null): RDD[T] = withScope {
+-    map(x => (x, null)).reduceByKey((x, y) => x, numPartitions).map(_._1)
++    // If the data is already approriately partioned with a known partioner we can work locally.
++    def removeDuplicatesInPartition(itr: Iterator[T]): Iterator[T] = {
++      val set = new mutable.HashSet[T]() ++= itr
++      set.toIterator
++    }
++    partitioner match {
++      case Some(p) if numPartitions == partitions.length =>
++        mapPartitions(removeDuplicatesInPartition, preservesPartitioning = true)
++      case _ => map(x => (x, null)).reduceByKey((x, y) => x, numPartitions).map(_._1)
++    }
+   }
+ 
+   /**
+diff --git a/core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala b/core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
+index b143a468a1baf..3001a2b005d8b 100644
+--- a/core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
++++ b/core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
+@@ -95,6 +95,18 @@ class RDDSuite extends SparkFunSuite with SharedSparkContext {
+     assert(!deserial.toString().isEmpty())
+   }
+ 
++  test("distinct with known partioner does not cause shuffle") {
++    val rdd = sc.parallelize(1.to(100), 10).map(x => (x % 10, x % 10)).sortByKey()
++    val initialPartioner = rdd.partitioner
++    val distinctRdd = rdd.distinct()
++    val resultingPartioner = distinctRdd.partitioner
++    assert(initialPartioner === resultingPartioner)
++    val distinctRddDifferent = rdd.distinct(5)
++    val distinctRddDifferentPartioner = distinctRddDifferent.partitioner
++    assert(initialPartioner != distinctRddDifferentPartioner)
++    assert(distinctRdd.collect().sorted === distinctRddDifferent.collect().sorted)
++  }
++
+   test("countApproxDistinct") {
+ 
+     def error(est: Long, size: Long): Double = math.abs(est - size) / size.toDouble
+
+From 5fd36592a26b07fdb58e79e4efbb6b70daea54df Mon Sep 17 00:00:00 2001
+From: Holden Karau <holden@pigscanfly.ca>
+Date: Fri, 10 Aug 2018 11:10:31 -0700
+Subject: [PATCH 2/7] CR feedback, reduce # of passes over data from 2 to 1 and
+ fix some spelling issues.
+
+---
+ core/src/main/scala/org/apache/spark/rdd/RDD.scala   |  6 +++---
+ .../test/scala/org/apache/spark/rdd/RDDSuite.scala   | 12 ++++++------
+ 2 files changed, 9 insertions(+), 9 deletions(-)
+
+diff --git a/core/src/main/scala/org/apache/spark/rdd/RDD.scala b/core/src/main/scala/org/apache/spark/rdd/RDD.scala
+index 471b9e0a1a877..d9eff9f9b0ac1 100644
+--- a/core/src/main/scala/org/apache/spark/rdd/RDD.scala
++++ b/core/src/main/scala/org/apache/spark/rdd/RDD.scala
+@@ -396,10 +396,10 @@ abstract class RDD[T: ClassTag](
+    * Return a new RDD containing the distinct elements in this RDD.
+    */
+   def distinct(numPartitions: Int)(implicit ord: Ordering[T] = null): RDD[T] = withScope {
+-    // If the data is already approriately partioned with a known partioner we can work locally.
++    // If the data is already approriately partitioned with a known partitioner we can work locally.
+     def removeDuplicatesInPartition(itr: Iterator[T]): Iterator[T] = {
+-      val set = new mutable.HashSet[T]() ++= itr"""
+  test("Slightlycomplexinput") {
+    val results = PatchExtractor.processPatch(slightlyComplexInput)
+    val commits = results.map(_.commitId).distinct
+    val expectedCommits = List("5fd36592a26b07fdb58e79e4efbb6b70daea54df",
+      "a7fbc74335c2df27002e8158f8e83a919195eed7")
+    val numAdded = results.filter(_.add).size
+    val numRemoved = results.filter(x => !x.add).size
+    commits should contain theSameElementsAs expectedCommits
+    numAdded should be (23)
+    numRemoved should be (3)
+    results should contain (PatchRecord("5fd36592a26b07fdb58e79e4efbb6b70daea54df",
+      399, 398,
+      "    // If the data is already approriately partioned with a known partioner we can work locally.",
+      false))
+  }
+}
