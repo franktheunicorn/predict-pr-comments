0910fd15c2f71a018717a0adc539f07af3e20740
Holden Karau <holden@pigscanfly.ca>
2019-01-24 13:57:53 -0800

    Run a few different periods.

--- rundev.sh
+++ rundev.sh
@@ -6,4 +6,7 @@ gcloud container clusters get-credentials tigeycluster --zone us-central1-a --pr
 # Upload the jar
 ./upload_spark_jar.sh 
 # Train the model
-SPARK_EXEC_MEMORY=18g VERSION=dev APP_NAME=ml1tester NUM_EXECS=20 SPARK_DEFAULT_PARALLELISM=30 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh 
+export APP_PREFX="ml3test-"
+SPARK_EXEC_MEMORY=18g VERSION=dev APP_NAME=$APP_PREFIX-$VERSION NUM_EXECS=20 SPARK_DEFAULT_PARALLELISM=30 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
+SPARK_EXEC_MEMORY=18g VERSION=2019 APP_NAME=$APP_PREFIX-$VERSION NUM_EXECS=20 SPARK_DEFAULT_PARALLELISM=30 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
+SPARK_EXEC_MEMORY=18g VERSION=2018 APP_NAME=$APP_PREFIX-$VERSION NUM_EXECS=20 SPARK_DEFAULT_PARALLELISM=30 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
