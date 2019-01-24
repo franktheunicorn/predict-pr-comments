5192c62d24c2e3dba34bc9a859dc4ebfc5f60639
Holden Karau <holden@pigscanfly.ca>
2019-01-23 23:43:54 -0800

    More aggressively de-dupe during point creation, add some logic to balance classes during training.

--- sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/ml/TrainingPipeline.scala
+++ sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/ml/TrainingPipeline.scala
@@ -10,7 +10,9 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.{Word2Vec, Tokenizer, StringIndexer, SQLTransformer, VectorAssembler}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
+import org.apache.spark.ml.feature.{Word2Vec, RegexTokenizer, StringIndexer, SQLTransformer, VectorAssembler}
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
@@ -19,6 +21,10 @@ import com.holdenkarau.predict.pr.comments.sparkProject.dataprep.{ResultData, Pa
 import com.holdenkarau.predict.pr.comments.sparkProject.helper.PatchExtractor
 
 case class LabeledRecord(text: String, filename: String, add: Boolean, commented: Boolean)
+// LabeledRecord + extension and label
+case class PreparedData(text: String, filename: String, add: Boolean, commented: Boolean,
+  extension: String, label: Double)
+
 
 class TrainingPipeline(sc: SparkContext) {
   val session = SparkSession.builder().getOrCreate()
@@ -40,11 +46,40 @@ class TrainingPipeline(sc: SparkContext) {
     labeledRecords.withColumn(
       "extension", extractExtensionUDF(labeledRecords("filename")))
       .withColumn(
-        "label", labeledRecords("commented").cast("double"))
+        "label", labeledRecords("commented").cast("double")).as[PreparedData]
+  }
+
+  def balanceClasses(input: Dataset[PreparedData]) = {
+    input.cache()
+    val (datasetSize, positives) = input.select(
+      count("*"), sum(input("label"))).as[(Long, Double)].collect.head
+    val balancingRatio = positives match {
+      case 0 => throw new Exception("No positive examples found, refusing to balance")
+      case _ => datasetSize / positives
+    }
+    // Some of the algs support weightCol some don't so just duplicate records
+    input.flatMap{record =>
+      if (record.label != 0.0) {
+        List.fill(balancingRatio.toInt)(record)
+      } else {
+        Some(record)
+      }
+    }
+  }
+
+  def trainAndEvalModel(input: Dataset[ResultData]) = {
+    val evaluator = new BinaryClassificationEvaluator()
+    // We have a pretty imbalanced class distribution
+      .setMetricName("areaUnderPR")
+      .setLabelCol("label")
+
   }
 
   def trainModel(input: Dataset[ResultData]) = {
     val preparedTrainingData = prepareTrainingData(input)
+    // Balanace the training data
+    val balancedTrainingData = balanceClasses(preparedTrainingData)
+
     val pipeline = new Pipeline()
     // Turn our different file names into string indexes
     val extensionIndexer = new StringIndexer()
@@ -53,7 +88,7 @@ class TrainingPipeline(sc: SparkContext) {
       .setOutputCol("extension_index")
     // For now we use the default tokenizer
     // In the future we could be smart based on programming language
-    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("tokens")
+    val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("tokens")
     // See the sorced tech post about id2vech - https://blog.sourced.tech/post/id2vec/
     val word2vec = new Word2Vec().setInputCol("tokens").setOutputCol("wordvecs")
     // Create our charlie brown christmasstree esque feature vector
@@ -66,9 +101,13 @@ class TrainingPipeline(sc: SparkContext) {
       extensionIndexer,
       tokenizer,
       word2vec,
+      // Todo: use the word2vec embeding to look for "typo" words ala https://medium.com/@thomasdecaux/build-a-spell-checker-with-word2vec-data-with-python-5438a9343afd
       featureVec,
       forest).toArray)
-    pipeline.fit(preparedTrainingData)
+    val model = pipeline.fit(balancedTrainingData)
+    val results = model.transform(preparedTrainingData)
+    results.show()
+    model
   }
 }
 
@@ -99,17 +138,17 @@ object TrainingPipeline {
     val initialRecords = patchLines.map(patchRecord =>
       LabeledRecord(patchRecord.text, patchRecord.filename, patchRecord.add,
         recordHasBeenCommentedOn(patchRecord))).distinct
-    //The same line may show up in multiple patches, if it's commented on in any of them
-    // we want to record it as commented
+    // The same text may show up in multiple places, if it's commented on in any of those
+    // we want to tag it as commented. We could do this per file but per PR for now.
     val commentedRecords = initialRecords.filter(_.commented)
     // Only output PRs which have a comment that we can resolve
     if (commentedRecords.isEmpty) {
       commentedRecords
     } else {
-      val seenTextAndFile = new HashSet[(String, String)]
+      val seenTextAndFile = new HashSet[String]
       val uncommentedRecords = initialRecords.filter(r => !r.commented)
       val resultRecords = (commentedRecords ++ uncommentedRecords).filter{
-        record => seenTextAndFile.add((record.text, record.filename))}
+        record => seenTextAndFile.add(record.text)}
       resultRecords
     }
   }
