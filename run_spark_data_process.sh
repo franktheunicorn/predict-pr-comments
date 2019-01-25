68172e32d0d219c620ad150963f7f287b346d458
Holden Karau <holden@pigscanfly.ca>
2019-01-24 18:22:44 -0800

    Default value of spark home

--- run_spark_data_process.sh
+++ run_spark_data_process.sh
@@ -14,6 +14,7 @@ export MEMORY_OVERHEAD_FRACT=${MEMORY_OVERHEAD_FRACK:="0.3"}
 export SPARK_DEFAULT_PARALLELISM=${SPARK_DEFAULT_PARALLELISM:="5000"}
 export APP_NAME=${APP_NAME:="spark-data-fetcher"}
 export MAIN_CLASS=${MAIN_CLASS:="com.holdenkarau.predict.pr.comments.sparkProject.DataFetchSCApp"}
+export SPARK_HOME=${SPARK_HOME:="~/Downloads/spark-2.4.0-bin-hadoop2.7/"}
 export RUNDATE=$(date +"%T")
 export APP_NAME="$APP_NAME-$RUNDATE"
 pushd $SPARK_HOME
