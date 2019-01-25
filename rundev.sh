92b9829e0b2e2318d7a31faef177471440e0c322
Holden Karau <holden@pigscanfly.ca>
2019-01-25 12:15:01 -0800

    Reduce memory to the point it can be scheduled

--- rundev.sh
+++ rundev.sh
@@ -7,7 +7,7 @@ gcloud container clusters get-credentials tigeycluster --zone us-central1-a --pr
 ./upload_spark_jar.sh 
 # Train the model
 export APP_PREFIX="ml4test"
-export SPARK_EXEC_MEMORY=21g
+export SPARK_EXEC_MEMORY=19g
 VERSION=dev APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=30 SPARK_DEFAULT_PARALLELISM=100 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
 VERSION=2019 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=40 SPARK_DEFAULT_PARALLELISM=200 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
 VERSION=2018 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=80 SPARK_DEFAULT_PARALLELISM=500 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml ./run_spark_data_process.sh
