a5d812cfc85daa2bf54c7968669f7b287d591bcf
Holden Karau <holden@pigscanfly.ca>
2019-01-24 18:30:49 -0800

    oooh -e might be causing that cause us to quit

--- upload_spark_jar.sh
+++ upload_spark_jar.sh
@@ -3,8 +3,10 @@ set -e
 set -x
 export COMMIT=$(git rev-parse HEAD)
 export TARGET=gs://frank-the-unicorn/jars/$COMMIT.jar
+set +e
 eval "gsutil stat $TARGET"
 if [ $? != 0 ]; then
+  set -e
   pushd sparkproject/
   sbt assembly
   gsutil cp ./target/scala-2.11/sparkProject-assembly-0.0.1.jar $TARGET
