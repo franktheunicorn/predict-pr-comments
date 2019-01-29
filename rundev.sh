e51bf9e42b0dc6512d35048f401b05cfe88560af
Holden Karau <holden@pigscanfly.ca>
2019-01-28 18:42:42 -0800

    Update the shell script

--- rundev.sh
+++ rundev.sh
@@ -6,8 +6,19 @@ gcloud container clusters get-credentials tigeycluster --zone us-central1-a --pr
 # Upload the jar
 ./upload_spark_jar.sh 
 # Train the model
-export APP_PREFIX="ml8dtest"
-VERSION=dev APP_NAME="$APP_PREFIX$VERSION" MEMORY_OVERHEAD_FRACT=0.35 NUM_EXECS=30 SPARK_DEFAULT_PARALLELISM=100 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX ./run_spark_data_process.sh
-#VERSION=2019 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=60 SPARK_DEFAULT_PARALLELISM=200 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX ./run_spark_data_process.sh
-#MEMORY_OVERHEAD_FRACT=0.35 VERSION=2018 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=80 SPARK_DEFAULT_PARALLELISM=300 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX ./run_spark_data_process.sh
-#MEMORY_OVERHEAD_FRACT=0.35 VERSION=full APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=100 SPARK_DEFAULT_PARALLELISM=1000 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX ./run_spark_data_process.sh
+export APP_PREFIX="ml9atest"
+# Kick off dev & full at the same time
+VERSION=dev APP_NAME="$APP_PREFIX$VERSION" MEMORY_OVERHEAD_FRACT=0.35 NUM_EXECS=30 SPARK_DEFAULT_PARALLELISM=100 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX CACHE=gs://frank-the-unicorn/$VERSION/ml/dataprep-cache ./run_spark_data_process.sh &
+piddev=$!
+MEMORY_OVERHEAD_FRACT=0.35 VERSION=full APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=100 SPARK_DEFAULT_PARALLELISM=1000 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX CACHE=gs://frank-the-unicorn/$VERSION/ml/dataprep-cache ./run_spark_data_process.sh &
+pidfull = $!
+wait $pidfull
+
+# Kick off our year-by-year versions after the fact
+
+VERSION=2019 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=60 SPARK_DEFAULT_PARALLELISM=200 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX CACHE=gs://frank-the-unicorn/$VERSION/ml/dataprep-cache ./run_spark_data_process.sh
+pid2019=$!
+MEMORY_OVERHEAD_FRACT=0.35 VERSION=2018 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=80 SPARK_DEFAULT_PARALLELISM=300 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX CACHE=gs://frank-the-unicorn/$VERSION/ml/dataprep-cache ./run_spark_data_process.sh &
+pid2018 = $!
+MEMORY_OVERHEAD_FRACT=0.35 VERSION=2017 APP_NAME="$APP_PREFIX$VERSION" NUM_EXECS=80 SPARK_DEFAULT_PARALLELISM=300 MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.ml.MlSCApp INPUT=gs://frank-the-unicorn/$VERSION/output OUTPUT=gs://frank-the-unicorn/$VERSION/ml-$APP_PREFIX CACHE=gs://frank-the-unicorn/$VERSION/ml/dataprep-cache ./run_spark_data_process.sh &
+pid2017 = $!
