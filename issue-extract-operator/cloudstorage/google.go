e749976cb3f6911da49a0f2b27de0f3497493074
Kris Nova <kris@nivenly.com>
2019-02-01 04:44:07 +0100

    Adding csv parser for issues

---
+++ issue-extract-operator/cloudstorage/google.go
@@ -0,0 +1,169 @@
+// Copyright © 2018 Kris Nova <kris@nivenly.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudstorage
+
+import (
+	"context"
+	"fmt"
+
+	"strings"
+
+	"sync"
+
+	"time"
+
+	"bytes"
+
+	"cloud.google.com/go/storage"
+	"github.com/kris-nova/logger"
+	"google.golang.org/api/option"
+)
+
+var WriterMutex sync.Mutex
+
+func NewGoogleCloudHandler(options *Options) (*GoogleCloudHandler, error) {
+	client, err := storage.NewClient(context.Background(), option.WithCredentialsFile(options.PathToAuthFile))
+	if err != nil {
+		return nil, fmt.Errorf("Unable to initalize new client: %v", err)
+	}
+	handler := &GoogleCloudHandler{
+		Client:  client,
+		Options: options,
+	}
+	return handler, nil
+}
+
+// ObjectExists can be used to see if an existing object already exists
+func (gc *GoogleCloudHandler) ObjectExists(objectName string) (bool, error) {
+	client := gc.Client
+	bucketHandler := client.Bucket(gc.Options.Bucket)
+	objects := bucketHandler.Objects(context.TODO(), &storage.Query{Prefix: objectName})
+	object, err := objects.Next()
+	if err != nil {
+		return false, nil
+	}
+	if object == nil {
+		return false, nil
+	}
+	return true, nil
+}
+
+// MutableSyncBytes will sync an object based on a given buffer
+func (gc *GoogleCloudHandler) MutableSyncBytes(buffer bytes.Buffer, objectname string) error {
+	client := gc.Client
+	options := gc.Options
+	bucketHandler := client.Bucket(options.Bucket)
+	logger.Always("Syncing to bucket: %s", options.Bucket)
+
+	// Now that the bucket has been ensured let's sync the buffer
+	objectHandler := bucketHandler.Object(objectname)
+	writer := objectHandler.NewWriter(context.TODO())
+
+	// Write to Google Cloud Storage writer with a mutex
+	logger.Always("Begin Atomic CSV Sync...")
+	WriterMutex.Lock()
+	writer.Write(buffer.Bytes())
+	writer.Close()
+	// Grab a time stamp after we close the network connection
+	rfc3339time := time.Now().Format(time.RFC3339)
+	WriterMutex.Unlock()
+
+	// attrs is hand for outputting meta information about the newly synced object
+	validated := false
+	for i := options.SyncWaitSecondAttempts; i >= 0; i-- {
+		_, err := objectHandler.Attrs(context.TODO())
+		if err != nil {
+			logger.Debug("Failure parsing object attributes: %v", err)
+			time.Sleep(time.Second * 1)
+			continue
+		} else {
+			validated = true
+			break
+		}
+	}
+	if validated {
+		// Atomicity is safe here
+		attrs, err := objectHandler.Update(context.TODO(), storage.ObjectAttrsToUpdate{
+			Metadata: map[string]string{
+				"Timestamp RFC3339": rfc3339time,
+				"Public":            "True",
+			},
+			// Grant read only to the public
+			ACL: []storage.ACLRule{
+				{
+					Entity: storage.AllUsers,
+					Role:   storage.RoleReader,
+				},
+			},
+		})
+		if err != nil {
+			logger.Warning("Unable to update meta information on object: %v", err)
+		}
+
+		logger.Always("Successful atomic transaction!")
+		logger.Always("Filename: %s", objectname)
+		logger.Always("Media Link: %s", attrs.MediaLink)
+		logger.Always("Bucket: %s", options.Bucket)
+		return nil
+	}
+	logger.Critical("Unable to validate eventually consistent object after %d attempts", options.SyncWaitSecondAttempts)
+	logger.Critical("Trying to recover...")
+	err := objectHandler.Delete(context.TODO())
+	if err != nil {
+		logger.Critical("Unable to recover: %v", err)
+		return fmt.Errorf("Uncrecoverable error cleaning up object: %v", err)
+	}
+	logger.Success("Recovered object %s", objectname)
+	return nil
+}
+
+// EnsureBucket will ensure a given bucket exists in an account
+func (gc *GoogleCloudHandler) EnsureBucket() error {
+	client := gc.Client
+	bucketName := gc.Options.Bucket
+	projectID := gc.Options.GoogleProject
+	bucketHandler := client.Bucket(bucketName)
+	found := false
+
+	buckets := client.Buckets(context.Background(), projectID)
+	for {
+		bucket, err := buckets.Next()
+		if err != nil && strings.Contains(err.Error(), "no more items in iterator") {
+			logger.Info("Finished scanning buckets", err)
+			break
+		} else if err != nil {
+			logger.Warning("Recoverable error scanning buckets: %v", err)
+			continue
+		}
+		if bucket == nil {
+			break
+		}
+		//logger.Info("Checking bucket: %s", bucket.Name)
+		if bucket.Name == bucketName {
+			logger.Success("Found existing bucket: %s", bucketName)
+			found = true
+			break
+		}
+	}
+	if found != true {
+		logger.Info("Bucket not found, creating bucket: %s versioning: true", bucketName)
+		err := bucketHandler.Create(context.Background(), projectID, &storage.BucketAttrs{VersioningEnabled: true})
+		if err != nil {
+			return fmt.Errorf("Error creating bucket: %s", bucketName)
+		}
+		logger.Success("Created bucket: %s", bucketName)
+	}
+	return nil
+}
---
+++ issue-extract-operator/cmd/cloud.go
@@ -0,0 +1,99 @@
+// Copyright © 2018 Kris Nova <kris@nivenly.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/holdenk/predict-pr-comments/issue-extract-operator/cloudstorage"
+
+	"github.com/holdenk/predict-pr-comments/issue-extract-operator/queery"
+	"github.com/kris-nova/logger"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	csoptions = &cloudstorage.Options{}
+	cloud     = ""
+)
+
+var cloudCmd = &cobra.Command{
+	Use:   "cloud",
+	Short: "Push the data to a cloud storage account",
+	Long:  `Use this command to start the operator in the Kubernetes mode and push data to a cloud storage account`,
+
+	// Run is the main entry point of the program
+	Run: func(cmd *cobra.Command, args []string) {
+
+		// Todo @kris-nova we have option bloat as per usual please do a consolidate
+		csoptions.PathToAuthFile = queeropts.PathToAuthFile
+		csoptions.GoogleProject = queeropts.GoogleProject
+
+		err := validate()
+		if err != nil {
+			logger.Critical("Failed validation: %v", err)
+			os.Exit(-1)
+		}
+		logger.Always("Using cloud: %s", cloud)
+		ds, err := queery.PullDataSet(queeropts)
+		if err != nil {
+			logger.Critical("Fatal error during query: %v", err)
+			os.Exit(1)
+		}
+
+		switch cloud {
+		case "gcs":
+			handler, err := cloudstorage.NewGoogleCloudHandler(csoptions)
+			if err != nil {
+				logger.Critical("Unable to create handler to cloud: %v", err)
+				os.Exit(98)
+			}
+			err = handler.EnsureBucket()
+			if err != nil {
+				logger.Critical("Unable to ensure bucket: %v", err)
+				os.Exit(97)
+			}
+			buffer, err := ds.ToCSV()
+			if err != nil {
+				logger.Critical("Unable to generate CSV: %v", err)
+				os.Exit(96)
+			}
+			// Note we do not check for existing here as this is the procederual script
+			err = handler.MutableSyncBytes(buffer, fmt.Sprintf("issue_urls/data_%d.csv", queeropts.CellLimit))
+			if err != nil {
+				logger.Critical("Unable to sync bytes: %v", err)
+				os.Exit(95)
+			}
+		default:
+			logger.Critical("Historical error: invalid cloud: %s", cloud)
+			os.Exit(99)
+		}
+	},
+}
+
+func init() {
+	cloudCmd.Flags().StringVarP(&cloud, "cloud", "c", "gcs", "The cloud storage account to use. Currently supported [GCS Google Cloud Storage]")
+	cloudCmd.Flags().StringVarP(&csoptions.Bucket, "bucket", "b", "tigeys-buckets-are-rad", "The cloud storage account to use. Currently supported [GCS Google Cloud Storage]")
+	cloudCmd.Flags().IntVarP(&csoptions.SyncWaitSecondAttempts, "sync-second-attempts", "N", 30, "When writing a new object to the cloud, we validate it's create. This defines how many single second attempts to wait for the eventually consistent API.")
+}
+
+func validate() error {
+	if cloud != "gcs" {
+		return fmt.Errorf("Invalid cloud: %s", cloud)
+	}
+	return nil
+}
