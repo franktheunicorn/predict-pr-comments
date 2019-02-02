c9770e9f60a47f15e89fb4dafdd6b57ff4f7d371
Holden Karau <holden@pigscanfly.ca>
2019-02-02 02:59:42 -0800

    Add more executors and parallelism

--- runissues.sh
+++ runissues.sh
@@ -2,4 +2,4 @@
 ./upload_spark_jar.sh 
 export MEMORY_OVERHEAD_FRACTION=0.40
 
-APP_NAME=ISSUES SPARK_DEFAULT_PARALLELISM=60 NUM_EXECS=10 INPUT="gs://tigeys-buckets-are-rad/issue_urls/*.csv" OUTPUT="gs://frank-the-unicorn/issues-full" MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.dataprep.IssueDataFetchSCApp ./run_spark_data_process.sh
+APP_NAME=ISSUES SPARK_DEFAULT_PARALLELISM=200 NUM_EXECS=50 INPUT="gs://tigeys-buckets-are-rad/issue_urls/*.csv" OUTPUT="gs://frank-the-unicorn/issues-full" MAIN_CLASS=com.holdenkarau.predict.pr.comments.sparkProject.dataprep.IssueDataFetchSCApp ./run_spark_data_process.sh
