aeb164375a46147c4c6f76addea5da49121ed61d
Holden Karau <holden@pigscanfly.ca>
2019-04-18 11:29:15 -0700

    Start updating to work on BQL dumps

--- sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/dataprep/DataTypes.scala
+++ sparkproject/src/main/scala/com/holdenkarau/predict/pr/comments/dataprep/DataTypes.scala
@@ -1,21 +1,23 @@
 package com.holdenkarau.predict.pr.comments.sparkProject.dataprep
 
 case class StoredPatch(pull_request_url: String, patch: String, diff: String)
-case class InputData(
-  pull_request_url: String,
+case class CommentInputData(
   pull_patch_url: String,
-  comments_positions_space_delimited: String,
-  comments_original_positions_space_delimited: String,
-  comment_file_paths_json_encoded: String,
-  comment_commit_ids_space_delimited: String)
-case class ParsedInputData(
+  created_date: Date,
+  pull_request_url: String,
+  comments_positions: Array[String],
+  diff_hunks: Array[String],
+  comments_original_positions: Array[String],
+  comment_commit_ids: Array[String],
+  comment_file_paths: Array[String])
+case class ParsedCommentInputData(
   pull_request_url: String,
   pull_patch_url: String,
   comments_positions: List[String],
   comments_original_positions: List[String],
   comment_file_paths: List[String],
   comment_commit_ids: List[String])
-case class ResultData(
+case class ResultCommentData(
   pull_request_url: String,
   pull_patch_url: String,
   // Some comments might not resolve inside of the updated or original spaces
@@ -24,8 +26,7 @@ case class ResultData(
   comments_original: List[Option[Int]],
   comment_file_paths: List[String],
   comment_commit_ids: List[String],
-  patch: String,
-  diff: String)
+  diff_hunks: Array[String])
 case class PatchRecord(commitId: String, oldPos: Int, newPos: Int,
   linesFromHeader: Option[Int], text: String, filename: String, add: Boolean)
 case class IssueInputRecord(name: String, url: String)
